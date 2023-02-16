614e5f6447c6d3ae6b6123f8d0700bb01058a23b
srinidhibalmuri <balmurisrinidhi@gmail.com>
2023-02-16 12:18:25 -0600

    in class

--- ImageViewDemo/ImageViewDemo/ViewController.swift
+++ ImageViewDemo/ImageViewDemo/ViewController.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let borderWidth = 0.25
+    let cornerRadius = 20.0
+    
+    let courses: Dictionary<String: Dictionary<String: String>> = [
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
